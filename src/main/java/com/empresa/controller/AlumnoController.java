e05e3cf0b8a650595f5fab8ad0b25ec24adf317e
kamalyzapana <kamaly@b89.io>
2021-06-02 19:52:23 -0500

    first commit

---
+++ src/main/java/com/empresa/controller/AlumnoController.java
@@ -0,0 +1,30 @@
+package com.empresa.controller;
+
+import java.util.List;
+
+import com.empresa.entity.Alumno;
+import com.empresa.service.AlumnoService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/alumno")
+public class AlumnoController {
+
+    @Autowired
+    private AlumnoService service;
+
+    @GetMapping
+    @ResponseBody
+    public ResponseEntity<List<Alumno>> listaAlumno(){
+        List<Alumno> lista = service.listaAlumno();
+        return ResponseEntity.ok(lista);
+
+    }
+
+}
